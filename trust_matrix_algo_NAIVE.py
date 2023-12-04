a5da95074a8c26adc70ee2935242590cef5c07ab
pippellia <108896743+pippellia-btc@users.noreply.github.com>
2023-12-04 14:43:25 +0100

    Create trust_matrix_algo_NAIVE.py

---
+++ trust_matrix_algo_NAIVE.py
@@ -0,0 +1,38 @@
+import numpy as np
+import scipy as sp
+from matplotlib import pyplot as plt
+import time
+
+# B is the belief matrix (bij = 1 if ni follows nj)
+# n is the number of iterations
+# gamma is how fast the trust decay factor
+
+def trust_matrix(B, n = 5, gamma = 0.5):
+
+    # Initialization
+    
+    # the trust matrix T at the beginning
+    T = np.identity(B.shape[0]) + B
+
+    # the list of indexes already counted, as a matrix
+    index_matrix = -T
+
+    # the previous power of B
+    B_prev_pow = B
+
+    # Loop
+    for i in range(n-1): #(j = 2, ... n)
+
+        # compute B^j and eliminate all the redundant indices        (which will have negative values)
+        B_new_pow = np.maximum(0, B_prev_pow @ B + index_matrix)
+        
+        # get all the new nonzero indices
+        index_matrix = index_matrix - B_new_pow
+        
+        # add the new matrix to the Trust matrix T
+        T = T + gamma**(i+1) * B_new_pow
+
+        # update the previous power of B
+        B_prev_pow = B_new_pow
+
+    return T
