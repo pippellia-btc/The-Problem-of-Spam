9f0f1dd517831dcf6847063741280b9bc1b96dad
pippellia <108896743+pippellia-btc@users.noreply.github.com>
2023-12-06 17:34:04 +0100

    Create trust_matrix_algo.py
    algorithm v0.2
    
    Instead of O(N^2.8) it works in O(N^2) because it leverages boolean efficient matrix multiplications

---
+++ v0.2/trust_matrix_algo.py
@@ -0,0 +1,34 @@
+# The difference this time is that the matrix B is boolean.
+# This means that the matrix product is no more O(N^2.8) using the Strassen Algorithm
+# but (on average), it is O(N^2) ---> extraordinary improvement!
+
+# the sum is replaced by the '|' (element-wise OR) operator
+# the subtraction '- index_matrix' is replaced by the  '& ~index_matrix' operator (AND NOT)
+
+def trust_matrix_bool(B, n = 5, gamma = 0.5):
+
+    # the list of indexes already counted, as a matrix
+    index_matrix = ~(np.identity(B.shape[0], dtype = bool) | B)
+    
+    # the trust matrix T at the beginning
+    T = (~index_matrix).astype(float)
+
+    # the current power of B
+    B_pow = B
+
+    # Loop
+    for i in range(n-1):
+
+        # compute B^j (j = 2, ... n), which becomes the current power of B
+        B_pow = B_pow @ B
+
+        # eliminate all the redundant indices
+        B_pow_star =  B_pow & index_matrix
+        
+        # include all the new indices in the index_matrix
+        index_matrix = index_matrix ^ B_pow_star
+        
+        # add the new matrix, multiplied by gamma^(j-1) to the Trust matrix T
+        T = T + gamma**(i+1) * B_pow_star
+
+    return T
